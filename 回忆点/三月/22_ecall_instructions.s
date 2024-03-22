5eebb309b97190980758d38aa89680548eaefc0a
WUADN <126257909+WUADN@users.noreply.github.com>
2024-03-22 14:27:10 +0800

    Update 22_ecall_instructions.s

--- 回忆点/三月/22_ecall_instructions.s
+++ 回忆点/三月/22_ecall_instructions.s
@@ -1,3 +1,5 @@
+# to print an integer, we need to make an ecall with a0 set to 1
+# the thing that will be printed is stored in register a1
 print_int:
      mv a1, a0
     # set register a0 to 1 so that the ecall will print
@@ -5,3 +7,13 @@ print_int:
     # print the integer
     ecall
 
+print_str:
+    mv a1, a0 #give the address of the string to a1 to point
+    li a0, 4 # tells ecall to print the string that a1 points to
+    ecall
+    jr    ra
+
+print_character:
+    li a1, '\n'
+    li a0, 11 # tells ecall to print the character in a1
+    ecall
